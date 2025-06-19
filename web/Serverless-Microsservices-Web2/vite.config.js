2f821a094acd291548ba2aa7f0c6da54ddea3ecb
DTerian64 <dterian64@outlook.com>
2025-06-19 14:23:00 -0700

    vite.config change-sourcemap: true

--- web/Serverless-Microsservices-Web2/vite.config.js
+++ web/Serverless-Microsservices-Web2/vite.config.js
@@ -11,6 +11,9 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
+   build: {
+    sourcemap: true  // ← Required
+  },
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {

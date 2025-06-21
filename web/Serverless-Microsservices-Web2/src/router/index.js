c6846674589c9f4406cb81104aee8b28fdca9b34
DTerian64 <dterian64@outlook.com>
2025-06-21 11:47:49 -0700

    Fixed no access route

--- web/Serverless-Microsservices-Web2/src/router/index.js
+++ web/Serverless-Microsservices-Web2/src/router/index.js
@@ -5,18 +5,31 @@ import Trip from '@/views/Trip.vue'
 import Drivers from '@/views/Drivers.vue'
 import Passengers from '@/views/Passengers.vue'
 import NoAuth from '@/views/NoAuth.vue'
+import store from '@/store';
 
 const routes = [
-  { path: '/', name: 'home', component: Home },
-  { path: '/trip', name: 'trip', component: Trip },
-  { path: '/drivers', name: 'drivers', component: Drivers },
-  { path: '/passengers', name: 'passengers', component: Passengers },
-  { path: '/noauth', name: 'noauth', component: NoAuth }
+  { path: '/', name: 'home', component: Home, meta: { requiresAuth: false }  },
+  { path: '/trip', name: 'trip', component: Trip, meta: { requiresAuth: true } },
+  { path: '/drivers', name: 'drivers', component: Drivers, meta: { requiresAuth: true } },
+  { path: '/passengers', name: 'passengers', component: Passengers, meta: { requiresAuth: true } },
+  { path: '/noauth', name: 'noauth', component: NoAuth, meta: { requiresAuth: false }  }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  console.log('to.meta', to.meta);
+  console.log('Redirecting to NoAuth:', to.fullPath);
+  const isAuthenticated = store.getters['common/user'] !== null;
+
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    next({ name: 'noauth' }); // Redirect to NoAuth.vue
+  } else {
+    next(); // Proceed normally
+  }
+});
 
 export default router
